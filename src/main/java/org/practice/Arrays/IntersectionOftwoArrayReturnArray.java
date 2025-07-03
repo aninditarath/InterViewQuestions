9fe0e6671843ce924a944b32f6ab5d0f1d824e02
anindita <anindita1193@gmail.com>
2025-07-03 20:56:08 +0530

    Commit : 3rd July
    Changed the existing code for findcommon in two arrays ,a simpler logic which removes duplicate

--- src/main/java/org/practice/Arrays/IntersectionOftwoArrayReturnArray.java
+++ src/main/java/org/practice/Arrays/IntersectionOftwoArrayReturnArray.java
@@ -1,53 +1,32 @@
 package org.practice.Arrays;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class IntersectionOftwoArrayReturnArray {
 
     public static int[] intersection(int[] nums1, int[] nums2) {
 
-        Map<Integer,Integer>mapp = new HashMap<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> result = new HashSet<>();
 
-        for(int i=0; i < nums1.length; i++){
+        for (int num : nums1) {
+            set1.add(num);
+        }
 
-            if(mapp.containsKey(nums1[i])){
-                mapp.put(nums1[i],mapp.get(nums1[i])+1);
-            }
-            else {
-                mapp.put(nums1[i],1);
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                result.add(num);
             }
         }
 
-        for(int i=0; i < nums2.length; i++){
-
-            if(mapp.containsKey(nums2[i])){
-                mapp.put(nums2[i],mapp.get(nums2[i])+1);
-            }
-            else {
-                mapp.put(nums2[i],1);
-            }
+        int[] output = new int[result.size()];
+        int index = 0;
+        for (int num : result) {
+            output[index++] = num;
         }
-
-        List<Integer> integerlist= new ArrayList<>();
-        for(Map.Entry<Integer,Integer>entry: mapp.entrySet()){
-
-            if(entry.getValue()>1){
-                integerlist.add( entry.getKey());
-
-            }
-
+        for(int k : output){
+            System.out.println(k);
         }
-
-        int[] intarrayvalues = new int[integerlist.size()];
-        for (int i = 0; i < integerlist.size(); i++)
-            intarrayvalues[i] = integerlist.get(i);
-        System.out.println("Array elements:");
-        for (int s : intarrayvalues) {
-            System.out.println(s);
-        }
-         return  intarrayvalues;
+        return output;
     }
 }
